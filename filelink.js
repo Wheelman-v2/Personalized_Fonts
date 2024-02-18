2dc08a87161b6bdc83b3b805909abefbe033716e
Wheelman-v2 <149758344+Wheelman-v2@users.noreply.github.com>
2024-02-18 14:45:07 +0900

    first commit

---
+++ filelink.js
@@ -0,0 +1,16 @@
+    function redirectToLink(linkType) {
+      var baseUrl = 'https://github.com/Wheelman-v2/Personalized_Fonts/raw/main';
+      var specificPaths = {
+        'drfont': 'drfont/drfont.zip',
+        // 'font2': 'AnotherFolder/another_font.zip',
+        // 필요에 따라 링크 추가
+      };
+
+      var specificPath = specificPaths[linkType];
+      if (specificPath) {
+        var fullUrl = baseUrl + '/' + specificPath;
+        window.location.href = fullUrl;
+      } else {
+        console.error('잘못된 링크 유형:', linkType);
+      }
+    }

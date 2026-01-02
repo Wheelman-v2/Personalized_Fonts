1898bc4b609c2878a106960747151a7f3caffa16
Wheelman-v2 <149758344+Wheelman-v2@users.noreply.github.com>
2026-01-02 23:36:28 +0900

    Update filelink.js

--- filelink.js
+++ filelink.js
@@ -2,6 +2,7 @@
       var baseUrl = 'https://github.com/Wheelman-v2/Personalized_Fonts/raw/main';
       var specificPaths = {
         'drfont': 'drfont/drfont.zip',
+        'Pilseung_Gothic': 'Pilseung_Gothic/필승고딕.zip',
         // 'font2': 'AnotherFolder/another_font.zip',
         // 필요에 따라 링크 추가
       };
@@ -13,4 +14,5 @@
       } else {
         console.error('잘못된 링크 유형:', linkType);
       }
+
     }
